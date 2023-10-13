e8b6901e95ae8905575d39827310c3e370fa9980
tercsam <mascret.clementgmail.com>
2023-10-13 07:28:58 -0700

    update c program 3

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints a line with the printf function
- * Return: 0 (success)
- *
+ * main - A program that print a line with the printf function
+ * return : 0 (success)
  */
 
 int main(void)
 {
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
