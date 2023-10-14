e8bf80de2573bb5bc3b873201e51291f237b6110
tercsam <mascret.clementgmail.com>
2023-10-14 04:22:25 -0700

    add program 3

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - this program print the size of varios type on the computer
+ * 
+ * Return : 0 (success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n" , (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n" , (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu bytes(s)\n" , (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n" , (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e)); 
+	return (0);
+}
+
