c8517adbdeeba89a3116707236467136f73a74ac
tercsam <mascret.clementgmail.com>
2023-10-14 04:32:13 -0700

    update script 3

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *  main - this program print the size of varios type on the computer
- * 
+ * main - this program print the size of varios type on the computer
+ *
  * Return : 0 (success)
  */
 int main(void)
@@ -13,11 +13,11 @@ int main(void)
 	char d;
 	float e;
 
-	printf("Size of a char: %lu byte(s)\n" , (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n" , (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu bytes(s)\n" , (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n" , (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e)); 
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
 
