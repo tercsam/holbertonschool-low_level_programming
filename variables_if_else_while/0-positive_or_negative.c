36bfb0dea9697757f77f60377c02a335be00c31d
tercsam <mascret.clementgmail.com>
2023-10-17 04:31:03 -0700

    update program 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n")
+		printf("%d is positive\n",n);
 	}
-	if (n = 0) 
+	if (n == 0)
 	{
-		printf("%d is zero\n")
+		printf("%d is zero\n",n);
 	}
 	if( n < 0)
 	{
-		printf("is negative\n")
+		printf("%d is negative\n",n);
 	}
 	return (0);
 }
