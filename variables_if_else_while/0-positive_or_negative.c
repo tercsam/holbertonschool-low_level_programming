63f52797bf879aeb867ae5a49ca8db2c935fbe94
tercsam <mascret.clementgmail.com>
2023-10-17 01:36:22 -0700

    add 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ * more headers goes there
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * betty style doc for function main goes there
+ */
+
 int main(void)
 {
 	int n;
