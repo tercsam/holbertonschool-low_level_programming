f957b277e3a3cf6090ec6acefa8c8d9f77eed709
tercsam <mascret.clementgmail.com>
2023-10-17 12:19:33 -0700

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 
 	Srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = srand() - RAND_MAX / 2;
 	if (n % 10 == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
