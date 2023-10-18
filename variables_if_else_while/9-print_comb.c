e2c01c9d7c9da5043294e202a591d5158a318aeb
tercsam <mascret.clementgmail.com>
2023-10-18 13:34:25 -0700

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,11 +11,13 @@ int main(void)
 	int number;
 
 	for (number = '0'; number <= '9'; number++)
+	{
 		putchar(number);
 
 		if (number < 9)
 			putchar(',');
 			putchar(' ');
+	}
 
 		putchar('\n');
 
