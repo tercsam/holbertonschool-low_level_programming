d91f45865be853405d34205d9f5ebce07397caf8
tercsam <mascret.clementgmail.com>
2023-10-18 12:47:55 -0700

    add program 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int number
+
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+
+	putchar('\n')
+
+	return (0);
+}
