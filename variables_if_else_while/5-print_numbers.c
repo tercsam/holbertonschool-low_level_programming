cfcbc23b4aa16f337c4af8b7e68e1c290ce6eae9
tercsam <mascret.clementgmail.com>
2023-10-18 06:53:11 -0700

    add program 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+
+	putchar('\n');
+
+	return (0);
+
+}
