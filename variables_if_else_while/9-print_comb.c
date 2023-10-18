6eeeb6c514b751fa0d7020d1a2e6f95f1b425a38
tercsam <mascret.clementgmail.com>
2023-10-18 13:25:23 -0700

    add program 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+
+		if (number < 9)
+			putchar(',');
+			putchar(' ');
+
+		putchar('\n');
+
+		return (0);
+
+}
