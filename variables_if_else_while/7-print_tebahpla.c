9330bcdac99e84a95ae659c91ce229c9751f13a4
tercsam <mascret.clementgmail.com>
2023-10-18 12:58:35 -0700

    add program 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
