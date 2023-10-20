bf4b0097e1e7b96218cf7d7ac34a465e52193259
root <mascret.clement@gmail.com>
2023-10-20 12:22:41 -0700

    update program 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry Point
@@ -9,7 +10,15 @@
 int main(void)
 
 {
-	puts("_putchar\n";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 
