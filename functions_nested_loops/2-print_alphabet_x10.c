ab725f4ac6a9c4099c05a12adb521a8804235806
root <mascret.clement@gmail.com>
2023-10-20 13:24:34 -0700

    add program 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return 0 (Success)
+ */
+
+int main(void)
+
+{
+	char letter;
+	int count = 0;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z', letter++)
+			putchar(letter)
+	}
+		putchar('\n')
+
+	}		
