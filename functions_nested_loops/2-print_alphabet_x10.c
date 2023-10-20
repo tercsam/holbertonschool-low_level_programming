e8a08a1467f95b4c7c8e6409403c97d739e633a4
root <mascret.clement@gmail.com>
2023-10-20 13:43:49 -0700

    .

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  * Return 0 (Success)
  */
 
-void print_alpahbet_x10(void)
+void print_alphabet_x10(void)
 
 {
 	char letter;
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void print_alphabet();
-void print_alpahbet_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 #endif /* MAIN_H */
 
