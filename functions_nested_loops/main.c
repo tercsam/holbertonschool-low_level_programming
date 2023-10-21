0b9ae14165eb4a12eefcf95b54d3d2a6df9400b3
root <mascret.clement@gmail.com>
2023-10-21 09:37:50 -0700

    update main.c

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,26 +1,11 @@
-#include "main.h"
-
 /**
- * test_islower - Test the _islower function
+ * _putchar - Entry point
+ * @c: The character to print
  *
- * @n: Number to pass to _islower function
+ * Return: 1 (Success)
  */
-void test_islower(int n)
-{
-	int r;
 
-	r = _islower(n);
-	_putchar(r + '0');
-	_putchar('\n');
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+int _putchar(char c)
 {
-	test_islower('H');
-	return (0);
+	return (write(1, &c, 1));
 }
