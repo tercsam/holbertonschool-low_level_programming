9b1d582b015a2379584314d21f7c0701d28791f5
root <mascret.clement@gmail.com>
2023-10-21 07:48:01 -0700

    add program 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _islower - Entry point
+ *
+ * @c - Checked character
+ *
+ * Return: 0 (Success)
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+		else if
+		{
+			return (0);
+		}
+
+}
