3ed1d8265bc4516dcce8274f248f8b7e5bcc65e2
root <mascret.clement@gmail.com>
2023-10-22 14:11:53 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @n: the full digit
+ * Return: return the last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+	a = n % 10;
+	if (a < 0)
+	{
+		a = -a;
+	}
+	_putchar(a + '0');
+	return (a);
+}
