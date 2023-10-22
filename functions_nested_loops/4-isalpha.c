c02639ae86c8a03beff44c36181076ae577564ce
root <mascret.clement@gmail.com>
2023-10-22 14:05:51 -0700

    add program 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Chechs letters
+ * @c: the checked character
+ * Return: 1 if c = letter ,else 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
