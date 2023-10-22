e8919989e39d0be51007e88afc2b60ec0089b00c
root <mascret.clement@gmail.com>
2023-10-22 14:02:09 -0700

    3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,12 +3,9 @@
 /**
  * _islower - Check if c is lowercase to return 1 if not return 0
  * @c: is the value that needs checking
- *
  * Return: 1 or 0
  */
-
 int _islower(int c)
-
 {
 	if (c >= 'a' && c <= 'z')
 	{
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,16 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _isalpha(int c);
-void print_alphabet();
+int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _putchar(char c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int)
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif /* MAIN_H */
