f039f4995f33f23c2b9c9a35dcba5c305d893465
root <mascret.clement@gmail.com>
2023-10-22 14:15:20 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting with 0.
+ *
+ * Return: Always 0
+ *
+ * c = colonne
+ * l = ligne
+ * r = resultat
+ * d = dizaine
+ * u = unité
+ *
+ */
+
+void times_table(void)
+{
+	int c;
+	int l;
+	int r;
+	int d;
+	int u;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(0 + '0');
+		for (l = 1; l <= 9; l++)
+		{
+			r = c * l;
+			u = r % 10;
+			d = r / 10;
+			if (d == 0)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(u + '0');
+			}
+			else if (d != 0)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+		}
+		_putchar('\n');
+	}
+
+}
