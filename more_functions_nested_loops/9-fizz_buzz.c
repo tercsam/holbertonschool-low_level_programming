fa0a8e5d4e8410f8b344621b2df47cc0a6022092
root <mascret.clement@gmail.com>
2023-10-22 13:53:11 -0700

    add program 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 100)
+	{
+		num++;
+		if ((num % 3) != 0 && (num % 5) != 0)
+		{
+			printf("%d", num);
+		}
+		else if ((num % 3)  == 0 && (num % 5) != 0)
+		{
+			printf("Fizz");
+		}
+		else if ((num % 3) != 0 && (num % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("FizzBuzz");
+		}
+		if (num < 100)
+			putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
