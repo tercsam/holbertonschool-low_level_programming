dd5df3909615216b907307f879c7a3d229b86966
root <mascret.clement@gmail.com>
2023-10-26 11:57:09 -0700

    add program 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to print
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+
+
+{
+	int length = 0;
+	int i = 0;
+
+	while (*(s + length) != '\0')
+	{
+	lenght++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+	_putchar(*(s + i));
+	}
+
+	_putchar('\n');
+
+}
