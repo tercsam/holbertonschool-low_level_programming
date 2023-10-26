85cbdd7510659a9a204610576174f03983661e61
root <mascret.clement@gmail.com>
2023-10-26 05:23:21 -0700

    add prog 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- *  * _strlen - returns the length of a string
- *   * @s: string to evaluate
- *    *
- *     * Return: the length of the string
- *      */
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 
 {
 	int i;
 
-	for (i = 0; s[i] != '\0' ; i++);
-	
+	for (i = 0; s[i] != '\0' ; i++)
+		;
+
 	return (i);
 
 }
