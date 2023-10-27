09535e9f088862d9634a207712f3ef30dc838143
root <mascret.clement@gmail.com>
2023-10-27 11:42:15 -0700

    update prog 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src + i) != '\0');
+	while (*(src[i])
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i]
 		i++
 	}
-	*(dest + i) = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
