8939f04db0b9572834bc78a17e5eebc4138d8013
root <mascret.clement@gmail.com>
2023-10-27 01:02:57 -0700

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Copy paste string
+ * @dest: destinaion
+ * @src: source
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src);
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
