4f8e29bcc60c9f3de9a52d6690ca62834df29cea
root <mascret.clement@gmail.com>
2023-10-27 11:59:27 -0700

    add program100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
+ * Return: the converted string.
+ */
+
+int _atoi(char *s)
+{
+	int x = 0;
+	int moins = 1;
+	unsigned int result = 0;
+
+	while (s[x] != '\0')
+	{
+		if (s[x] >= '0' && s[x] <= '9')
+		{
+			break;
+		}
+		if (s[x] == '-')
+		{
+			moins *= -1;
+		}
+		x++;
+	}
+	while (s[x] >= '0' && s[x] <= '9')
+	{
+		result = (result * 10) + (s[x] - '0');
+		x++;
+		if (s[x] < '0' || s[x] > '9')
+		{
+			result *= moins;
+			return (result);
+		}
+	}
+	return (result);
+}
