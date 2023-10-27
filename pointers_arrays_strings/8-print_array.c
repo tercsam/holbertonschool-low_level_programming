88a3240923a7fe111847f18107ac54ad4569f7f6
root <mascret.clement@gmail.com>
2023-10-27 00:34:11 -0700

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,11 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-		if (i != (n - 1))
-		{
-			printf(", ");
-		}
+	for (int i = 0; i < n; i++)
+		  printf("%d", a[i]);
+	  if (i < n - 1)
+		  printf(", ");
 	}
-		_putchar('\n');
-
+	printf("\n")
 }
