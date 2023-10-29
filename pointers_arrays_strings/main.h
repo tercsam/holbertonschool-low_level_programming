4ae077d5f77507e8301ff132edd059b8afc6d8e9
root <mascret.clement@gmail.com>
2023-10-29 13:52:39 -0700

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,3 +19,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
