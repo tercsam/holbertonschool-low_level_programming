d9f4316a6c28834f72de6affbe34fe81c8f4d509
root <mascret.clement@gmail.com>
2023-11-02 03:16:32 -0700

    add prog 2

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: the string to be printed
+ *
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+	else
+		putchar('\n');
+
+}
