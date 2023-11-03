6f72e1a7e319168b446440b9fd4d56ea92faa165
root <mascret.clement@gmail.com>
2023-11-03 13:10:50 -0700

    add prog 3

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+
+	if (argc >= 0)
+
+{
+	print("%s\n", argv[i]);
+}
+	return (0);
+}
