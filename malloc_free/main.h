3c95cc9015be3944a7305f4038137b004155779b
root <mascret.clement@gmail.com>
2023-11-10 08:29:28 -0800

    main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
@@ -7,5 +5,3 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-
-#endif
