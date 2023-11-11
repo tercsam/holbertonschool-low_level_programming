95ea12bc1ff7d66b62fc6511902df44a5ff723f2
root <mascret.clement@gmail.com>
2023-11-11 05:41:47 -0800

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,8 +8,8 @@
  * @s2: The string to be concatenated to s1.
  *
  * Return: If concatenation fails - NULL.
- * 	Otherwise - a pointer the newly-allocated space in memory
- *                     containing the concatenated strings.
+ *	Otherwise - a pointer the newly-allocated space in memory
+ *             containing the concatenated strings.
  */
 char *str_concat(char *s1, char *s2)
 {
