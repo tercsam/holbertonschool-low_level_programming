2d5bd096801d64dcad8d84bfbae57dc6cc7487fc
root <mascret.clement@gmail.com>
2023-11-12 14:39:50 -0800

    2,3

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the name of the file it was compiled from
- *   * Return: 0
- *    */
+ * main - prints the name of the file it was compiled from
+ * Return: 0
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
