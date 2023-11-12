0e0b5ce8abe3108a780807b2f0a15d9e56d36898
root <mascret.clement@gmail.com>
2023-11-12 14:19:00 -0800

    prog 4

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- *  * array_range - creates an array of integers
- *   * @min: first integer in array
- *    * @max: last integer in array
- *     *
- *      * Return: pointer to new array, or NULL on failure
- *       */
+ * array_range - creates an array of integers
+ * @min: first integer in array
+ * @max: last integer in array
+ *
+ * Return: pointer to new array, or NULL on failure
+ */
 int *array_range(int min, int max)
 {
 	int *a;
