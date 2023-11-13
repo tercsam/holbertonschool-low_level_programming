5c3992005bed1d4445be939c4aee3f5ab7f9d7cf
root <mascret.clement@gmail.com>
2023-11-13 01:55:08 -0800

    create Dir + README txt + first header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
