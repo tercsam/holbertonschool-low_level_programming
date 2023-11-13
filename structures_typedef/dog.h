d35c1bbe221baf8815e6006015053fec084976ca
root <mascret.clement@gmail.com>
2023-11-13 08:55:47 -0800

    update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,13 +8,13 @@
  * @owner: Third member
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
