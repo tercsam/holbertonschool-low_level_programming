1e3b6496e6bf43b82952102524e3ffeb36794692
tercsam <135607601+tercsam@users.noreply.github.com>
2024-01-05 15:41:10 +0100

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,99 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#define BUFFER_SIZE 1024
+#include "main.h"
 
 /**
- * closeFile - Close file descriptor and exit on failure.
- * @fd: File descriptor to close.
- * @filename: Name of the file associated with the file descriptor.
- */
-void closeFile(int fd, const char *filename)
+ * __exit - prints error messages and exits with exit value
+ * @error: is either exit value or file descriptor
+ * @s: it's a name, either of the two filenames
+ * @fd: file descriptor
+ * Return: 0 on success
+ **/
+int __exit(int error, char *s, int fd)
 {
-	if (close(fd) == -1)
+	switch (error)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd for %s\n", filename);
-		exit(100);
+	case 97:
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(error);
+	case 98:
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+		exit(error);
+	case 99:
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+		exit(error);
+	case 100:
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(error);
+	default:
+		return (0);
 	}
 }
 
+
 /**
- * copyFile - Copy content from source file to destination file.
- * @src: File descriptor of the source file.
- * @dest: File descriptor of the destination file.
- * @av: Command-line arguments.
+ * main - copy one file to another file
+ * @argc: should be or are 3
+ * @argv: first is file to copy from fd_1, second is file to copy to fd_2
+ * Return: 0 success, else exit value errors
  */
-
-void copyFile(int src, int dest, char **av)
+int main(int argc, char *argv[])
 {
-	char buffer[BUFFER_SIZE];
-	ssize_t bytesRead, bytesWritten;
-
-	while ((bytesRead = read(src, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytesWritten = write(dest, buffer, bytesRead);
-		if (bytesWritten == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
-	}
-
-	if (bytesRead == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-}
-/**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array of command-line argument strings
- *
- * Description:
- *   - Validates the number of command-line arguments.
- *   - Opens the source file in read-only mode.
- *   - Handles errors if unable to open the source file.
- *   - Opens or creates the destination file in write-only
- *   - mode, truncating if it exists.
- *   - Handles errors if unable to open or create the destination file.
- *   - Copies the content from the source file to the destination file.
- *   - Closes the source and destination files.
- *
- * Return: Always 0 (indicating successful execution).
- */
-int main(int argc, char **argv)
-{
-	int src;
-	int dest;
+	int fd_1, fd_2, n_read, n_write;
+	char *buffer[1024];
 
 	if (argc != 3)
+		__exit(97, NULL, 0);
+
+	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd_2 == -1)
+		__exit(99, argv[2], 0);
+
+	fd_1 = open(argv[1], O_RDONLY);
+	if (fd_1 == -1)
+		__exit(98, argv[1], 0);
+
+	while ((n_read = read(fd_1, buffer, 1024)) != 0)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
+		if (n_read == -1)
+			__exit(98, argv[1], 0);
+
+		n_write = write(fd_2, buffer, n_read);
+		if (n_write == -1)
+			__exit(99, argv[2], 0);
 	}
 
-	src = open(argv[1], O_RDONLY, 0664);
-	if (src == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (dest == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		closeFile(src, argv[1]);
-		exit(99);
-	}
-
-	copyFile(src, dest, argv);
-
-	closeFile(src, argv[1]);
-	closeFile(dest, argv[2]);
-
+	close(fd_2) == -1 ? (__exit(100, NULL, fd_2)) : close(fd_2);
+	close(fd_1) == -1 ? (__exit(100, NULL, fd_1)) : close(fd_1);
 	return (0);
 }
