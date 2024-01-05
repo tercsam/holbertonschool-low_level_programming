6a59da570407e4262e55bd71a58ecee31b629590
tercsam <135607601+tercsam@users.noreply.github.com>
2024-01-05 15:45:49 +0100

    Update 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,26 +1,36 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - create a hash table
- * @size: size of the array
- * Return: a pointer to the newly created hash table, NULL if failure
+ * hash_table_create - create hash table given size
+ * @size: size
+ * Return: pointer to table; NULL if error
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	unsigned long int i;
+	hash_node_t **array;
+	unsigned long int d = 0;
+
+	if (size == 0)
+		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (!ht)
 		return (NULL);
+	array = malloc(sizeof(*array) * size);
+	if (!array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	while (d < size)
+	{
+		array[d] = NULL;
+		d++;
+	}
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	ht->array = array;
 
 	return (ht);
 }
