8a4b63f8394303fd4950dcb01356f34253456920
tercsam <135607601+tercsam@users.noreply.github.com>
2024-01-05 15:43:45 +0100

    Update 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -2,37 +2,31 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: file's name
- * @text_content: NULL terminated string to add at the end of the file
- * Return: 1 on success, -1 on failure
- */
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end
+ * Return: 1 on success and -1 on failure
+ **/
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, nletters, rwr;
+	int fd, d;
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (nletters = 0; text_content[nletters];)
-			nletters++;
-	}
+	if (text_content == NULL)
+		return (1);
 
-	rwr = write(fd, text_content, nletters);
+	for (d = 0; text_content[d] != 0; d++)
+		;
 
-	if (rwr == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-
+	d = write(fd, text_content, d);
 	close(fd);
+	if (d == -1)
+		return (-1);
 	return (1);
 }
