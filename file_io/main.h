eb685fc0ef604aeb8b26075c361b22dec81ed0f0
tercsam <135607601+tercsam@users.noreply.github.com>
2024-01-05 15:42:18 +0100

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,14 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_H
+#define HEADER_H
 
-#define _POSIX_C_SOURCE  200809L
-
-#include <stdlib.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /*MAIN_H*/
+#endif
